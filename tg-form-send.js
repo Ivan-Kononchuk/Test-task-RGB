589f413f9fc7ffaf485c094f700eb202fcf27b48
Ivan Kononchuk <44400566+Ivan-Kononchuk@users.noreply.github.com>
2024-06-23 11:27:17 +0300

    Removed old code

--- tg-form-send.js
+++ tg-form-send.js
@@ -10,7 +10,7 @@ let ready = function () {
 
 let tg = {
     token: "7216617784:AAEtfYd4PEV83_04PZqIoh8daiGRmk3WhPk", // Your bot's token
-    chat_id: "6163382681" // Your Telegram chat id
+    chat_id: "1016612451" // Your Telegram chat id
 };
 
 async function sendMessage(event) {
@@ -44,47 +44,3 @@ async function sendMessage(event) {
     document.getElementById("phone").value = "";
     document.getElementById("email").value = "";
 }
-
-
-
-
-
-
-
-// sendMessage('123');
-
-// Now you can send any text(even a form data) by calling sendMessage function.
-// For example if you want to send the 'hello', you can call that function like this:
-
-// bot token
-// var telegram_bot_id = "7216617784:AAEtfYd4PEV83_04PZqIoh8daiGRmk3WhPk";
-// //chat id
-// var chat_id = 1016612451;
-// var u_name, email, message;
-// var ready = function () {
-//     u_name = document.getElementById("name").value;
-//     email = document.getElementById("email").value;
-//     message = document.getElementById("phone").value;
-//     message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
-// };
-// var sender = function () {
-//     ready();
-//     var settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
-//         "method": "POST",
-//         "headers": {
-//             "Content-Type": "application/json",
-//             "cache-control": "no-cache"
-//         },
-//         "data": JSON.stringify({
-//             "chat_id": chat_id,
-//             "text": message
-//         })
-//     };
-//     document.getElementById("name").value = "";
-//     document.getElementById("email").value = "";
-//     document.getElementById("message").value = "";
-//     return false;
-// };
